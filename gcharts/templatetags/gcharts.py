0164abc602863706c7f7ffcfcdbdd46ab9dc56b4
Rolf Håvard Blindheim <rhblind@gmail.com>
2012-10-04 22:56:52 +0200

    support for defining package type in render tag

--- gcharts/templatetags/gcharts.py
+++ gcharts/templatetags/gcharts.py
@@ -8,11 +8,20 @@
 from django.conf import settings
 from django import template
 from django.template.loader import render_to_string
+from django.template.base import TemplateSyntaxError
+from django.core.exceptions import ImproperlyConfigured
 
 register = template.Library()
 
+GOOGLECHARTS_PACKAGES = ["corechart", "gauge", "geochart", "table", "treemap"]
+
 _api = getattr(settings, "GOOGLECHARTS_API", "1.1")
 _packages = getattr(settings, "GOOGLECHARTS_PACKAGES", ["corechart"])
+
+for pkg in _packages:
+    if pkg not in GOOGLECHARTS_PACKAGES:
+        raise ImproperlyConfigured("%s is not a valid package. Valid packages are" \
+                                   % (pkg, ", ".join(GOOGLECHARTS_PACKAGES)))
 
 def _remove_quotes(s):
     if s[0] in ('"', "'") and s[-1] == s[0]:
@@ -30,8 +39,12 @@ class GChartsNode(template.Node):
     
     def render(self, context):
         js = self._nodelist.render(context)
+        pkg = context.get("pkg", None)
+        packages = [p for p in _packages]
+        if (pkg is not None and pkg not in packages):
+            packages.append(pkg)
         return self.render_template("gcharts/gcharts.html", googlecharts_js=js,
-                                    api=_api, packages=_packages)
+                                    api=_api, packages=packages)
 
 @register.tag
 def gcharts(parser, token):
@@ -70,11 +83,12 @@ def options(parser, token):
 # {% render "container_id" "data" "options" %}
 
 class RenderNode(template.Node):
-    def __init__(self, container, data, options):
+    def __init__(self, container, data, options, pkg):
         self.container = container
         self.data = template.Variable(data)
         self.options = options
-    
+        self.pkg = pkg
+        
     def render(self, context):
         """
         opt = _clone(googlecharts_options_%(options)s);
@@ -83,17 +97,24 @@ class RenderNode(template.Node):
             %(data)s
         googlecharts.push(opt);
         """
+        if self.pkg is not "default":
+            if self.pkg in GOOGLECHARTS_PACKAGES:
+                context["pkg"] = self.pkg
+            else:
+                raise TemplateSyntaxError("%s is not a valid package. Valid packages are %s" \
+                                          % (self.pkg, ", ".join(GOOGLECHARTS_PACKAGES)))
+
         return self.render.__doc__ % {"options": self.options, "container": self.container,
                                       "src": self.data.var, "data": self.data.resolve(context)}
 
 @register.tag
 def render(parser, token):
     args = token.split_contents()
-    if len(args) < 2:
-        raise template.TemplateSyntaxError('%r tag requires at least one argument' % args[0])
-    while len(args) < 4:
-        args.append('default')
-    _, container, data, options = [_remove_quotes(s) for s in args]
-    return RenderNode(container=container, data=data, options=options)
+    if len(args) < 4:
+        raise template.TemplateSyntaxError('%r tag requires at least three arguments' % args[0])
+    while len(args) < 5:
+        args.append("default")
+    _, container, data, options, pkg = [_remove_quotes(s) for s in args]
+    return RenderNode(container=container, data=data, options=options, pkg=pkg)
 
 
